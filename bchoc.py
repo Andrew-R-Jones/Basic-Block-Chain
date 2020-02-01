42916fad6573b919aface0bf081027776ef66fac
Andrew Jones <Andrew_52490@yahoo.com>
2020-02-01 16:55:01 -0700

    Update bchoc.py

--- bchoc.py
+++ bchoc.py
@@ -1,78 +1,138 @@
-
 #!/usr/bin/python3
 
 import sys
 
-class BlockChain:
+# global list that will hold all blocks and will create the chain
+chain = []
 
-    # should be ran at init run, with args passed in from cmd line
-    def __init__(self, *args):
-
-        # takes the cmd line inputs
-        # removes filename, and set the args as a list
-        list_of_commands = args[0][1:]
-
-        ''' print for testing command input '''
-        print(list_of_commands)       
-
-    '''
-    bchoc add -c case_id -i item_id [-i item_id ...]
-    Add a new evidence item to the blockchain and associate it with the given case identifier.
-    For users’ convenience, more than one item_id may be given at a time, which will create a 
-    blockchain entry for each item without the need to enter the case_id multiple times. The state 
-    of a newly added item is CHECKEDIN. The given evidence ID must be unique 
-    (i.e., not already used in the blockchain) to be accepted.
-    '''
-    def add():
-        return None
+'''
+bchoc add -c case_id -i item_id [-i item_id ...]
+Add a new evidence item to the blockchain and associate it with the given case identifier.
+For users’ convenience, more than one item_id may be given at a time, which will create a
+blockchain entry for each item without the need to enter the case_id multiple times. The state
+of a newly added item is CHECKEDIN. The given evidence ID must be unique
+(i.e., not already used in the blockchain) to be accepted.
+'''
 
 
-    '''
-    bchoc checkout -i item_id
-    Add a new checkout entry to the chain of custody for the given evidence item. Checkout actions 
-    may only be performed on evidence items that have already been added to the blockchain.
-    '''
-    def checkout():
-        return None
-
-    '''
-    bchoc checkin -i item_id
-    Add a new checkin entry to the chain of custody for the given evidence item. Checkin actions may
-    only be performed on evidence items that have already been added to the blockchain.
-    '''
-    def checkin():
-        return None
-
-    '''
-    bchoc log [-r] [-n num_entries] [-c case_id] [-i item_id]
-    Display the blockchain entries giving the oldest first (unless -r is given).
-    '''
-    def log():
-        return None
-
-    '''
-    bchoc remove -i item_id -y reason [-o owner]
-    Prevents any further action from being taken on the evidence item specified. The specified
-    item must have a state of CHECKEDIN for the action to succeed.
-    '''
-    def remove():
-        return None
-
-    '''
-    bchoc init
-    Sanity check. Only starts up and checks for the initial block.
-    '''
-    def init():
-
-        return None
-
-    '''
-    bchoc verify
-    Parse the blockchain and validate all entries.
-    '''
-    def verify():
-        return None
+def add():
+    print("add function")
+    return None
 
 
-#initial call from command line
-blockchain = BlockChain(sys.argv)
+'''
+bchoc checkout -i item_id
+Add a new checkout entry to the chain of custody for the given evidence item. Checkout actions
+may only be performed on evidence items that have already been added to the blockchain.
+'''
+
+
+def checkout():
+    print("checkout function")
+    return None
+
+
+'''
+bchoc checkin -i item_id
+Add a new checkin entry to the chain of custody for the given evidence item. Checkin actions may
+only be performed on evidence items that have already been added to the blockchain.
+'''
+
+
+def checkin():
+    print("checkin function")
+    return None
+
+
+'''
+bchoc log [-r] [-n num_entries] [-c case_id] [-i item_id]
+Display the blockchain entries giving the oldest first (unless -r is given).
+'''
+
+
+def log():
+    print("log function")
+    return None
+
+'''
+bchoc remove -i item_id -y reason [-o owner]
+Prevents any further action from being taken on the evidence item specified. The specified
+item must have a state of CHECKEDIN for the action to succeed.
+'''
+
+def remove():
+    print("remove function")
+    return None
+
+'''
+bchoc init
+Sanity check. Only starts up and checks for the initial block.
+'''
+
+def init():
+    print("init function")
+    return None
+
+'''
+bchoc verify
+Parse the blockchain and validate all entries.
+'''
+def verify():
+    print("verify function")
+    return None
+
+
+''' runs certain function based on the command argument passed in '''
+def run_commands(command):
+    if command == 'add':
+        add()
+    elif command == 'checkout':
+        checkout()
+    elif command == 'checkin':
+        checkin()
+    elif command == 'log':
+        log()
+    elif command == 'remove':
+        remove()
+    elif command == 'init':
+        init()
+    elif command == 'verify':
+        verify()
+   
+# saves the list of blocks to a file
+def save_to_file():
+    # for testing
+    blocks = ['block11', 'block22', 'block33', 'block44']
+
+    with open('blockchain.txt', 'w') as filehandle:
+        for listitem in blocks:
+            filehandle.write('%s\n' % listitem)
+
+# reads and restores the saved blocks from file, and add to list 'chain'
+def read_from_file():
+
+    # define an empty list
+    # open file and read the content in a list
+    with open('blockchain.txt', 'r') as filehandle:
+        for line in filehandle:
+            # remove linebreak which is the last character of the string
+            item = line[:-1]
+
+            # add item to the list
+            chain.append(item)
+
+
+# initial call from command line
+# ensures enough args given
+if len(sys.argv) < 2:
+    print('not enough arguments')
+    exit()
+
+# gets the command line arguments
+commands = sys.argv[1:]
+
+# calls function the run first command
+run_commands(commands[0])
+
+
+
