b1766fddc697da47e0c375a4ce761e14a693f2b6
azheat112 <japete19@asu.edu>
2020-02-07 08:53:18 -0700

    Add files via upload
    updated struct need help with variable string

--- jared_test1.py
+++ jared_test1.py
@@ -23,33 +23,32 @@ case_id_back = int(case_id_back)
 evidence_id = 123456
 state = "CHECKEDIN"
 state = bytes(state, 'utf-8')
-length = 20
 data = "just gonna leave this hear"
+length = len(data)
 data = bytes(data,'utf-8')
-block_test = pack('sdqqisis',hash,timestamp,case_id_front,case_id_back,evidence_id,state,length,data)
-a,b,c,d,e,f,g,h = unpack('sdqqisis', block_test)
+#block_test = pack('20sdqqi11si26s',hash,timestamp,case_id_front,case_id_back,evidence_id,state,length,data)
+#a,b,c,d,e,f,g,h = unpack('20sdqqi11si26s', block_test)
+block_test = pack('20sdqqi11si26s',hash,timestamp,case_id_front,case_id_back,evidence_id,state,length,data)
+a,b,c,d,e,f,g,h = unpack('20sdqqi11si26s', block_test)
 print("a: ", a, " b: ", b, " c:", c, " d:", d, " e:" ,e, "f: " ,f, "g:" , g, "h: ", h)
-string = "Python"
 
-# string with encoding 'utf-8'
-arr = bytes(string, 'utf-8')
-print(arr)
-arr1 = arr.decode("utf-8")
-print(arr1)
-tester = pack('10s',arr)
-ret = unpack('10s',tester)
-#print(ret1)
-print("doing some bit shifting")
-str5 = "123456"
-str1 = str5[:3]
-str2 = str5[3:]
-str2 = int(str2)
-str1 = int(str1)
-test = pack('qq', str1,str2)
-j,k = unpack('qq',test)
-print("j:", j, " k:", k)
-comb = str(j) + str(k)
+#unpack
+unpack_hash = a.decode("utf-8")
+unpack_timestamp = b
+unpack_case_id = str(case_id_front) + str(case_id_back)
+print("hash decode: ", unpack_hash)
+print("timestamp decode", unpack_timestamp)
+print("case_id decode: ", unpack_case_id)
 
-print("str1: ", str1, "binar: ", bin(str1))
-print("str2: ", str2, "binar: ", bin(str2))
-print("combined ", comb)
+#I need help with the string conversion I am strugling understanding it
+#On stack overflow I found a solution but I dont understand % workings
+#to get the struct completed I need to add padding(easy), double check the bytes are correct
+# and finish variable length string"data" below is a working example of variable length string packed. I dont
+# know how to unpack I am confused by the %.
+
+str5 = "going to test this"
+str5 = bytes(str5,'utf-8')
+str5_len = len(str5)
+print("str5_len", str5_len)
+val = pack('i%ds' % (len(str5),), len(str5), str5)
+#unpack_val = unpack('i%ds',val)  #this doesn't work
