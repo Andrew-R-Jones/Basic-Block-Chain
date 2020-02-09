9e59da4339107376ad7ad2a4036f6d033c3b8cc1
Andrew Jones <Andrew_52490@yahoo.com>
2020-02-09 13:49:04 -0700

    refactor block.py
    removed item_id from constructor. arguments.
    fixed spelling error in output.

--- block.py
+++ block.py
@@ -4,7 +4,7 @@ import maya
 class Block: 
 
     # instantiates block for blockchain
-    def __init__(self, previous_hash, case_id, item_id, evidence_item_id, data):
+    def __init__(self, previous_hash, case_id, evidence_item_id, data):
         self.previos_hash = previous_hash
         self.case_id = case_id
         self.evidence_item_id = evidence_item_id
@@ -15,8 +15,8 @@ class Block:
 
     ''' similar to toString, returns info about the object'''
     def __repr__(self):
-        return f"BLOCK INFORMATION\nPrevious Hash: {self.previos_hash}\nTimestamp: {self.Timestamp}\nCase ID: {self.case_id}\nEvidence Item ID: {self.item_id}\nStae: {self.state}\nData Length: {self.data_length} Bytes\nData: {self.data}"
+        return f"BLOCK INFORMATION\nPrevious Hash: {self.previos_hash}\nTimestamp: {self.Timestamp}\nCase ID: {self.case_id}\nEvidence Item ID: {self.evidence_item_id}\nState: {self.state}\nData Length: {self.data_length} Bytes\nData: {self.data}"
 
 # test data for creating new block
-b = Block('65cc391d-6568-4dcc-a3f1-86a2f04140f3', '987654321', 1234, 852963, "this is the data section.")
+b = Block('65cc391d-6568-4dcc-a3f1-86a2f04140f3', '987654321', 852963, "this is the data section.")
 print(b)
